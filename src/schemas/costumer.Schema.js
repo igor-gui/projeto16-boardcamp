757a87b7c04644fdf7665cdb0a1d30ee47d20a90
Igor Guimarães <igorgui.tec@gmail.com>
2023-05-15 17:02:41 -0300

    JOI CORRECTION

--- src/schemas/costumer.Schema.js
+++ src/schemas/costumer.Schema.js
@@ -2,8 +2,8 @@ import Joi from 'joi';
 
 const customerSchema = Joi.object({
   name: Joi.string().required(),
-  phone: Joi.number().required(),
-  cpf: Joi.number().required(),
+  phone: Joi.string().required(),
+  cpf: Joi.string().required(),
   birthday: Joi.date().required(),
 });
 
--- src/schemas/game.Schema.js
+++ src/schemas/game.Schema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const gameSchema = Joi.object({
     name: Joi.string().required(),
-    image: Joi.string().uri().required(),
+    image: Joi.string().required(),
     stockTotal: Joi.number().positive().required(),
     pricePerDay: Joi.number().positive().required(),
   });
