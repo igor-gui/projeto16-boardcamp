bf03edac6881e599988a9c25374c13197cef2b8d
Igor Guimarães <igorgui.tec@gmail.com>
2023-05-15 17:13:06 -0300

    schema

--- src/schemas/game.Schema.js
+++ src/schemas/game.Schema.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const gameSchema = Joi.object({
     name: Joi.string().required(),
     image: Joi.string().required(),
-    stockTotal: Joi.number().positive().required(),
-    pricePerDay: Joi.number().positive().required(),
+    stockTotal: Joi.number().greater(0).required(),
+    pricePerDay: Joi.number().greater(0).required(),
   });
